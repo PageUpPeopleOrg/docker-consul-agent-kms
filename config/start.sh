fa6b54f28ee8e5e5c062a1cb707ac20f53d5f254
Brendan Spinks <brendanspinks@gmail.com>
2016-06-24 14:44:55 +1000

    added local ip

--- config/start.sh
+++ config/start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-/bin/consul agent -node=$NODE -config-dir=/config -dc=$DC -atlas-join -atlas=$ACCOUNT -atlas-token=`shush decrypt $TOKEN`
+export LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+/bin/consul agent -node=$NODE -config-dir=/config -dc=$DC -atlas-join -atlas=$ACCOUNT -atlas-token=`shush decrypt $TOKEN` -advertise $LOCAL_IP
